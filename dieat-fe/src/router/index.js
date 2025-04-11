5432aee18138aec364275532d8b2c6489ae66039
50071 <rkd50071@gmail.com>
2025-04-11 20:03:11 +0900

    [공통] App.vue, main.js, router/index.js에 searchFood, readFree 추가

--- dieat-fe/src/router/index.js
+++ dieat-fe/src/router/index.js
@@ -5,17 +5,19 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
 import Signup from '@/views/Signup.vue';
 import Login from '@/views/Login.vue';
-import Service from '@/views/Service.vue';
 // import About from '@/pages/About.vue';
 // import Contact from '@/pages/Contact.vue';
+import SearchFood from '@/features/searchFood/pages/FoodSearchPage.vue'
+import ReadFree from '@/features/readFree/pages/FreePostListPage.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    {path: '/', name: Home, component: Home},
-    {path: '/signup', component: Signup},
-    {path: '/login', component: Login}
-
+    { path: '/', name: Home, component: Home },
+    { path: '/signup', component: Signup },
+    { path: '/login', component: Login },
+    { path: '/searchFood', component: SearchFood },
+    { path: '/readFree', component: ReadFree }
   ]
 });
 
